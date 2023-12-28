a0ccde4770b55a20cda82098ad30fb660d0e073b
mohammadjury <93785257+mohammadjury@users.noreply.github.com>
2023-12-28 11:03:23 +0200

    Update app.js

--- app.js
+++ app.js
@@ -2,11 +2,14 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { Sequelize } = require('sequelize');
-const userProfilesRoutes = require('./routes/userProfilesRoutes');
-const authRoutes = require('./routes/authRoutes');
 const environmentalDataRoutes = require('./routes/environmentalData');
 const communityReportsRoutes = require('./routes/communityReports');
+const educationalResourceRoutes = require('./routes/educationalResourceRoutes');
 const environmentalAlertsRoutes = require('./routes/environmentalAlertsRoutes');
+const sustainabilityScoresRoutes = require('./routes/sustainabilityScoresRoutes');
+const userProfilesRoutes = require('./routes/userProfilesRoutes');
+const authRoutes = require('./routes/authRoutes');
+const openDataAccessRoutes = require('./routes/opendataaccessRoutes');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -21,17 +24,24 @@ const sequelize = new Sequelize('ecoTrack', 'root', '', {
 });
 
 // Models
-const UserProfiles = require('./models/UserProfiles')(sequelize);
 const EnvironmentalData = require('./models/EnvironmentalData')(sequelize);
 const CommunityReports = require('./models/CommunityReports')(sequelize);
+const EducationalResource = require('./models/EducationalResource')(sequelize);
 const EnvironmentalAlerts = require('./models/EnvironmentalAlerts')(sequelize);
+const SustainabilityScore = require('./models/SustainabilityScore')(sequelize);
+const UserProfiles = require('./models/UserProfiles')(sequelize);
+const OpenDataAccess = require('./models/OpenDataAccess')(sequelize);
 
 // Routes
-app.use('/api/userProfiles', userProfilesRoutes(UserProfiles));
-app.use('/api/auth', authRoutes(UserProfiles)); 
 app.use('/api/environmentalData', environmentalDataRoutes(EnvironmentalData));
 app.use('/api/communityReports', communityReportsRoutes(CommunityReports));
+app.use('/api/educationalResources', educationalResourceRoutes(EducationalResource));
 app.use('/api/environmentalAlerts', environmentalAlertsRoutes(EnvironmentalAlerts));
+app.use('/api/sustainabilityscores', sustainabilityScoresRoutes(SustainabilityScore));
+app.use('/api/userProfiles', userProfilesRoutes(UserProfiles));
+app.use('/api/auth', authRoutes(UserProfiles)); 
+app.use('/api/opendataaccess', openDataAccessRoutes(OpenDataAccess));
+
 
 // Server setup
 sequelize
